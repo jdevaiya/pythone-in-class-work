08177507f8630c6317303b1cb8f64d6ba1121e41
j devaiya <159694837+jdevaiya@users.noreply.github.com>
2024-02-19 21:45:55 +0530

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,1 +1,15 @@
-
+# program of comparison operation
+a=6
+b=2
+# equals to operator
+print("a==b",a==b)
+# not equal
+print("a!=b",a!=b)
+#grater than 
+print("a>b",a>b)
+#less than 
+print("a<b",a<b)
+#grater than equal to
+print("a>=b",a>=b)
+#less than  equal to
+print("a<=b",a<=b)
